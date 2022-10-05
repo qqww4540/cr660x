c42997ae4915d17ce50f73fd29ed9718827e63c1
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2022-10-05 17:13:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/XiaoMi/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/XiaoMi/g' package/base-files/files/bin/config_generate
