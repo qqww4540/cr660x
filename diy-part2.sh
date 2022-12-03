d57dd3bc51cceb059a50e8bb3e488c18978ccd85
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2022-12-03 17:13:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,5 @@
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 # sed -i 's/OpenWrt/XiaoMi/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/XiaoMi/g' package/base-files/files/bin/config_generate
+curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
+curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
